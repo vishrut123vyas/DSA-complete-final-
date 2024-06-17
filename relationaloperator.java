7879ef18bc9b486e14289392790a0dabd764bf0d
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-06-17 11:46:41 +0530

    Add files via upload

---
+++ relationaloperator.java
@@ -0,0 +1,25 @@
+public class relationaloperator {
+    public static void main(String[] args) {
+        int a =10;
+        int b =100;
+    System.out.println((a>=b));
+    }
+}
+
+
+
+// public class relationaloperator {
+//     public static void main(String[] args) {
+//         int a =10;
+//         int b =100;
+//     System.out.println((a!=b));
+//     }
+// }
+
+//  public class relationaloperator {
+//     public static void main(String[] args) {
+//         int a =10;
+//     int b =10;
+//     System.out.println((a==b));
+//     }
+// }
