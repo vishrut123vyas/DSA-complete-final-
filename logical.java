c980300b8658dee82e7de9bd1bf773209994d4a6
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-06-18 11:15:45 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,20 @@
+package operators;
+// public class logical {
+//     public static void main(String[] args) {
+//         System.out.println((3>4)||(9<8));
+//     }
+// }
+
+
+// public class logical {
+//     public static void main(String[] args) {
+//         System.out.println((3<4)&&(9<8));
+//     }
+// }
+
+public class logical {
+        public static void main(String[] args) {
+            System.out.println(!(3>4));
+        }
+    }
+    
