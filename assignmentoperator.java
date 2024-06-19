4516b54e8d1de02bb18b0c3ae0e44b11ce81af5a
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-06-19 12:02:14 +0530

    Add files via upload

---
+++ assignmentoperator.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class assignmentoperator {
+    public static void main(String[] args) {
+        int a=10;
+        // a= a+10;
+        a+=10;
+        int b=15;
+        // b=b-5;
+        b-=5;
+       b*=5; // b=b*5
+
+        System.out.println(b);
+    }
+}
