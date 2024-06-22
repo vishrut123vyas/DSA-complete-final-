a1bac2af1e19296ae20c24b830e94bfbc485daef
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-06-22 12:45:42 +0530

    Add files via upload

---
+++ ternaryoperator.java
@@ -0,0 +1,15 @@
+public class ternaryoperator {
+    public static void main(String args[]){
+        int marks=89;
+         
+        String reportcard = marks>=33? "pass": "fail" ;
+        System.out.println(reportcard);
+    
+
+
+
+
+
+    } 
+}
+
