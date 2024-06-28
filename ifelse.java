6e77be48facde2340fbc60ebfaab599ac7385d97
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-06-28 12:30:12 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+public class ifelse {
+        public static void main(String[] args){
+         int age=19;
+         if(age>=18){
+            System.out.println("eligeble to vote");
+         }
+        else if (age>16&&15>=age){
+            System.out.println("he/she can vote");
+         }
+         else{
+            System.out.println("not eligeble");
+         }
+        }
+}
+    
+
