9155e34ffae35181da172f1a65385ea4306cf78a
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-07-01 11:17:42 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,31 @@
+// import java.util.*;
+
+// public class switchcase{
+//     private static final int case1 = 0;
+
+//     public static void main(String args[]){
+//         int number=4;
+//         switch(number){
+//             case1 : System.out.println("pizza");
+//                 break;
+//             case2 : System.out.println("burger");
+//                  break;
+//             case3 : System.out.println("mango shake");
+//                 break;
+//             default : System.out.println("ghar pe roti khale");
+//     }
+// }
+public class switchcase {
+    public static void main(String[] args) {
+        int number = 4;
+        switch (number) {
+            case 1: System.out.println("pizza");
+                break;
+            case 2: System.out.println("burger");
+                break;
+            case 3: System.out.println("mango shake");
+                break;
+            default: System.out.println("ghar pe roti khale");
+        }
+    }
+}
