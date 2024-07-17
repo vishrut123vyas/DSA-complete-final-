50fafd2451aaf5c633b6432ffa03f7312200ee4a
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-07-17 21:56:14 +0530

    Add files via upload

---
+++ forpattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/**
+ * forpattern
+ */
+public class forpattern {
+
+    public static void main(String[] args) {
+        for (int line = 1; line <= 4; line++) {
+            System.out.println("****");
+        }
+    }
+}
