aaf91482c6e79e8992243b0f3a3272c8ed73cb7f
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-07-23 12:03:41 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+import java.util.*;
+/**
+ * dowhile
+ */
+public class dowhile {
+
+    public static void main(String[] args) {
+        int n=1;
+        do {
+            System.out.println("jai shree ram");
+            n++;
+        } while (n<=5);
+    }
+}
