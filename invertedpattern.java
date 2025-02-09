dab0a080d0452e5f7986616fca2ab2340cb8b555
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2025-02-09 15:38:00 +0530

    Add files via upload

---
+++ invertedpattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/**
+ * invertedpattern
+ */
+public class invertedpattern {
+
+    public static void main(String[] args) {
+        for (int line = 1; line <=4; line++) {
+            for (int star = 1; star <=(4-line+1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
