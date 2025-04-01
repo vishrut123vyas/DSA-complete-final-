b1452aa83dde6a226c6f657b8b1c4394aa20e40a
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2025-04-01 23:03:27 +0530

    Add files via upload

---
+++ halfpyramid.java
@@ -0,0 +1,30 @@
+// import java.util.*;
+// /**
+//  * halfpyramid
+//  */
+// public class halfpyramid {
+
+//     public static void main(String[] args) {
+//         int n=4;
+//         for (int i = 1; i <=n; i++) {
+//             for (int j = 1; j <=i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+import java.util.*;
+
+public class halfPyramid {
+
+    public static void main(String[] args) {
+        int n = 4;
+        for (int line = 1; line <= n; line++) {
+            for (int num = 1; num <= i; num++) {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
